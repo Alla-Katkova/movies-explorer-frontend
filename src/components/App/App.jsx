72f17e34f22e3a543e268045e3982a7f0d048783
Kot <katkova.alla@gmail.com>
2023-09-19 16:46:52 +0300

    prettier установака

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,11 @@
-import logo from '../../logo.svg';
-import './App.css';
-import FakeHeader from '../FakeHeader/FakeHeader.jsx';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import FakeHeader from "../FakeHeader/FakeHeader.jsx";
 
 function App() {
   return (
     <div className="App">
-      <FakeHeader/>
-
+      <FakeHeader />
     </div>
   );
 }
