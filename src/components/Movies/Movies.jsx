d4546006fca17067d674642026c728e4212a245b
Kot <katkova.alla@gmail.com>
2023-10-11 22:22:57 +0300

    откатываю ошибочный коммит , чтобы не коммитить в main Revert "c сервера подружаются фильмы и сохраненные фильмы, настроен прелоадер и настроена длтельность фильмов"
    This reverts commit e42a7c1b5e3102fa61c7cb2a5fd9cdc059276d8c.

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,54 +3,15 @@ import MoviesCardList from "../MoviesCardList/MoviesCardList.jsx";
 import SearchForm from "../SearchForm/SearchForm.jsx";
 import Header from "../Header/Header.jsx";
 import Footer from "../Footer/Footer.jsx";
-import React, { useEffect, useState } from "react";
-import Preloader from "../Preloader/Preloader";
-import { fetchMovies } from "../../utils/MoviesApi";
-import { fetchSavedMovies } from "../../utils/MainApi";
-
-function addSavedFlag(movies, savedMovies) {
-  return movies.map((movie) => {
-    const savedMovie = savedMovies.find((s) => s.id === movie.id);
-    if (savedMovie) {
-      return { ...movie, isSaved: true };
-    }
-    return { ...movie, isSaved: false };
-  });
-}
+import { fakeCards } from "../../utils/constants";
 
 export default function Movies(props) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [movies, setMovies] = useState([]);
-  const [savedMovies, setSavedMovies] = useState([]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    Promise.all([fetchMovies(), fetchSavedMovies()])
-      .then(([moviesFromServer, savedMoviesFromServer]) => {
-        setMovies(addSavedFlag(moviesFromServer, savedMoviesFromServer));
-        setSavedMovies(savedMoviesFromServer);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
   return (
     <>
       <Header />
       <main className="movies">
         <SearchForm />
-        <details>
-          <summary>Movies</summary>
-          <pre>{JSON.stringify(movies, null, 2)}</pre>
-        </details>
-        <details>
-          <summary>Saved Movies</summary>
-          <pre>{JSON.stringify(savedMovies, null, 2)}</pre>
-        </details>
-        {isLoading ? <Preloader /> : null}
-        <MoviesCardList moviesData={movies} />
+        <MoviesCardList moviesData={fakeCards} />
         <div className="movies__add-button-container">
           <button type="button" className="movies__add-button">
             Ещё
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,11 +2,14 @@ import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard.jsx";
 
 export default function MoviesCardList({ moviesData }) {
+  function randomTrueOrFalse() {
+    return Math.random() < 0.5;
+  }
   return (
     <section className="movies-list">
       <ul className="movies-list__cataloges">
         {moviesData.map((movie) => (
-          <MoviesCard isSaved={movie.isSaved} movie={movie} key={movie.title} />
+          <MoviesCard isSaved={randomTrueOrFalse()} movie={movie} key={movie.title} />
         ))}
       </ul>
     </section>
